2c1f0370b5979c66db48686f247311df1602d158
Afrie Irham <32460534+afrieirhamakram@users.noreply.github.com>
2017-12-10 01:16:41 +0800

    Lab 8 Question 5 (Burger)

--- Lab 8/Question 6/Lab 8 Question 6/src/lab/pkg8/question/pkg6/Burger.java
+++ Lab 8/Question 6/Lab 8 Question 6/src/lab/pkg8/question/pkg6/Burger.java
@@ -10,5 +10,30 @@ package lab.pkg8.question.pkg6;
  * @author Afrie Irham
  */
 public class Burger {
-    
+
+    private String stallName;
+    private String id;
+    private int noBurgerSold;
+    public static int totalSold;
+
+  public Burger(String stallName, String id, int noBurgerSold){
+    this.stallName = stallName;
+    this.id = id;
+    this.noBurgerSold = noBurgerSold;
+    totalSold += noBurgerSold;
+  }
+
+  public void BurgerSold(int noBurgerSold){
+    this.noBurgerSold += noBurgerSold;
+    totalSold += noBurgerSold;
+  }
+
+  public void showSold(){
+    System.out.println("The number of burger sold by " + stallName + " is " + noBurgerSold);
+  }
+
+  public void showAllSold(){
+    System.out.println("The total number of burger sold is " + totalSold);
+  }
+
 }
--- Lab 8/Question 6/Lab 8 Question 6/src/lab/pkg8/question/pkg6/Lab8Question6.java
+++ Lab 8/Question 6/Lab 8 Question 6/src/lab/pkg8/question/pkg6/Lab8Question6.java
@@ -15,7 +15,21 @@ public class Lab8Question6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+      Burger burger1 = new Burger("Afrie", "123456", 900);
+      Burger burger2 = new Burger("Irham", "234567", 700);
+      Burger burger3 = new Burger("Akram", "345678", 50);
+
+      burger1.BurgerSold(100);
+      burger2.BurgerSold(300);
+      burger3.BurgerSold(50);
+
+      burger1.showSold();
+      burger2.showSold();
+      burger3.showSold();
+
+      burger1.showAllSold();
+
     }
-    
+
 }
