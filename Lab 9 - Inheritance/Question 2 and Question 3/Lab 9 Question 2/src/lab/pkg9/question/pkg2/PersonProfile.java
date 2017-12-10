027cba8600e6f6908da9ca199adc1a87c865fc98
Afrie Irham <32460534+afrieirhamakram@users.noreply.github.com>
2017-12-10 19:17:17 +0800

    Lab 9 Question 2

--- Lab 9 - Inheritance/Question 2 and Question 3/Lab 9 Question 2/src/lab/pkg9/question/pkg2/PersonProfile.java
+++ Lab 9 - Inheritance/Question 2 and Question 3/Lab 9 Question 2/src/lab/pkg9/question/pkg2/PersonProfile.java
@@ -10,5 +10,28 @@ package lab.pkg9.question.pkg2;
  * @author Afrie Irham
  */
 public class PersonProfile {
-    
+
+  protected String name;
+  protected char gender;
+  protected String birthDate;
+
+  public PersonProfile(){
+
+  }
+
+  public void showName(){
+    System.out.println("Name: " + name);
+  }
+
+  public void showGender(){
+    System.out.println("Gender: " + gender);
+  }
+
+  public void showBirthday(){
+    System.out.println("Date of Birth: " + birthDate);
+  }
+
+
+
+
 }
